d439adf26e48a767181c5d55b32615fc4e24e1a3
Jonathan <dos.santos.2k23@gmail.com>
2025-07-06 09:29:47 -0300

    feat: create NewItem widget in new_item.dart. Also set a button that bring the user from GroceryList to it

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_items.dart';
 
-class GroceryList extends StatelessWidget {
+import 'package:shopping_list/data/dummy_items.dart';
+import 'new_item.dart';
+
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+    void _addItem() {
+      Navigator.of(
+        context,
+      ).push(MaterialPageRoute(builder: (ctx) => NewItem()));
+    }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Your Groceries')),
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: () {
+          _addItem();
+        }, icon: Icon(Icons.add))],
+      ),
       body: ListView.builder(
         itemCount: groceryItems.length,
         itemBuilder:
