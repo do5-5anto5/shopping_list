51fd5ba106069af9387b2f12a919c4b080efd1ea
Jonathan <dos.santos.2k23@gmail.com>
2025-07-07 18:17:12 -0300

    feat: Parsing response.body

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shopping_list/models/grocery_item.dart';
 
+import '../data/categories.dart';
 import '../sec/secrets.dart';
 import 'new_item.dart';
 
@@ -13,18 +16,41 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
 
   @override
   void initState() {
-        super.initState();
-        _loadItems();
+    super.initState();
+    _loadItems();
   }
 
   void _loadItems() async {
     final url = Uri.https(baseUrl, 'shopping-list.json');
     final response = await http.get(url);
-    print (response.body);
+    final Map<String, dynamic> listData = json.decode(
+      response.body,
+    );
+    final List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+      final category =
+          categories.entries
+              .firstWhere(
+                (catItem) => catItem.value.title == item.value['category'],
+              )
+              .value;
+      loadedItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
+    }
+
+    setState(() {
+      _groceryItems = loadedItems;
+    });
   }
 
   void _addItem() async {
